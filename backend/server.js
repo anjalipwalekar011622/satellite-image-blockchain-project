85f1e1607741a057e7cf8129c348367e3fae58dc
ANUSHREE VERMA <202304061.anushreekml@student.xavier.ac.in>
2026-03-14 12:35:26 +0000

    backend server initialized with express

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+    res.send("Backend server is running");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
